2236fd9ddc7e423feb5116cb421f5679143362ad
anuj123-45 <anuj1358.be20@chikara.edu.in>
2023-06-11 22:45:37 +0530

    try catch added

--- controllers/productContoller.js
+++ controllers/productContoller.js
@@ -1,3 +1,5 @@
+const mongoose=require('mongoose');
+
 const Product = require('../models/productModel');
 
 
@@ -6,12 +8,18 @@ const Product = require('../models/productModel');
 // create  product
 
 exports.createProduct = async (req, res, next) => {
+
+  try{
   const product = await Product.create(req.body);
 
   res.status(201).json({
     success: true,
     product
   })
+}
+catch(err){
+  return res.json({error:err});
+}
 
 }
 
@@ -23,15 +31,21 @@ exports.createProduct = async (req, res, next) => {
 
 
 exports.getAllProducts = async (req, res) => {
+  try{
   const products = await Product.find();
   res.status(200).json({ succes: true, products });
-
+  }
+  catch(err){
+    return res.json({error:err});
+  }
 }
 
 
 // update product -- Admin
 
 exports.updateProduct = async (req, res,next) => {
+  
+  try{
 
   let product = await Product.findById(req.params.id);
 
@@ -48,5 +62,49 @@ exports.updateProduct = async (req, res,next) => {
   })
 
   res.status(200).json({ success: true, product });
+  }
+  catch(err){
+    return res.json({error:err});
+  }
+}
+
+// delete route
+
+exports.deleteProduct=async(req,res)=>{
+  try{
+  const product=await Product.findOneAndDelete({_id:req.params.id});
+  if(!product){
+    return res.status(500).json({success:false,message:"Product not exists"});
+  }
+
+  else {
+    return res.status(200).json({success:true,message:"Product deleted successfully"});
+  }
+}
+catch(err){
+  return res.json({error:err});
+}
+
+}
+
+// get product details   (specific)
+
+exports.getSpecificProduct=async(req,res)=>{
+
+  try{
+
+  const product=await Product.findById(req.params.id);
+  
+  if(!product){
+    return res.status(500).json({success:false,message:"Product not exists"});
+  }
+
+  else {
+    return res.status(200).json({success:true,product});
+  }
+  }
+  catch(err){
+    return res.json({error:err});
+  }
 
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const { getAllProducts ,createProduct, updateProduct } = require('../controllers/productContoller');
+const { getAllProducts ,createProduct, updateProduct, deleteProduct, getSpecificProduct } = require('../controllers/productContoller');
 
 
 const router=express.Router();
@@ -8,7 +8,7 @@ const router=express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
-router.route("/product/update/:id").put(updateProduct);
+router.route("/product/update/:id").put(updateProduct).delete(deleteProduct).get(getSpecificProduct);
 
 
 module.exports=router;
