8f31ee5fd4d42ef6d1d88c987b53c62ee22646e1
anuj123-45 <anuj1358.be20@chikara.edu.in>
2023-06-12 07:50:30 +0530

    search , filter and pagination done

--- controllers/productContoller.js
+++ controllers/productContoller.js
@@ -1,6 +1,7 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 const Product = require('../models/productModel');
+const ApiFeatures = require('../utils/apifeatures');
 
 
 
@@ -9,17 +10,17 @@ const Product = require('../models/productModel');
 
 exports.createProduct = async (req, res, next) => {
 
-  try{
-  const product = await Product.create(req.body);
+  try {
+    const product = await Product.create(req.body);
 
-  res.status(201).json({
-    success: true,
-    product
-  })
-}
-catch(err){
-  return res.json({error:err});
-}
+    res.status(201).json({
+      success: true,
+      product
+    })
+  }
+  catch (err) {
+    return res.json({ error: err });
+  }
 
 }
 
@@ -31,80 +32,83 @@ catch(err){
 
 
 exports.getAllProducts = async (req, res) => {
-  try{
-  const products = await Product.find();
-  res.status(200).json({ succes: true, products });
+  try {
+    const resultPerPage = 5;
+    const productCount=await Product.countDocuments();
+    const apifeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
+    const products = await apifeature.query;
+    res.status(200).json({ succes: true, products ,productCount});
   }
-  catch(err){
-    return res.json({error:err});
+  catch (err) {
+    return res.json({ error: err });
   }
 }
 
 
 // update product -- Admin
 
-exports.updateProduct = async (req, res,next) => {
-  
-  try{
+exports.updateProduct = async (req, res, next) => {
 
-  let product = await Product.findById(req.params.id);
+  try {
 
-  if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found"
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(500).json({
+        success: false,
+        message: "Product not found"
+      })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+
     })
+
+    res.status(200).json({ success: true, product });
   }
-
-  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-
-  })
-
-  res.status(200).json({ success: true, product });
-  }
-  catch(err){
-    return res.json({error:err});
+  catch (err) {
+    return res.json({ error: err });
   }
 }
 
 // delete route
 
-exports.deleteProduct=async(req,res)=>{
-  try{
-  const product=await Product.findOneAndDelete({_id:req.params.id});
-  if(!product){
-    return res.status(500).json({success:false,message:"Product not exists"});
-  }
+exports.deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findOneAndDelete({ _id: req.params.id });
+    if (!product) {
+      return res.status(500).json({ success: false, message: "Product not exists" });
+    }
 
-  else {
-    return res.status(200).json({success:true,message:"Product deleted successfully"});
+    else {
+      return res.status(200).json({ success: true, message: "Product deleted successfully" });
+    }
   }
-}
-catch(err){
-  return res.json({error:err});
-}
+  catch (err) {
+    return res.json({ error: err });
+  }
 
 }
 
 // get product details   (specific)
 
-exports.getSpecificProduct=async(req,res)=>{
+exports.getSpecificProduct = async (req, res) => {
 
-  try{
+  try {
 
-  const product=await Product.findById(req.params.id);
-  
-  if(!product){
-    return res.status(500).json({success:false,message:"Product not exists"});
-  }
+    const product = await Product.findById(req.params.id);
 
-  else {
-    return res.status(200).json({success:true,product});
+    if (!product) {
+      return res.status(500).json({ success: false, message: "Product not exists" });
+    }
+
+    else {
+      return res.status(200).json({ success: true, product });
+    }
   }
-  }
-  catch(err){
-    return res.json({error:err});
+  catch (err) {
+    return res.json({ error: err });
   }
 
 }
